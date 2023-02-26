1d75d129a7cb543f5c3e60015f1351e561afdabc
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-02-26 17:37:31 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
-
+import DogPics from './DogPics'
 function App() {
   useEffect(() => {
     console.log("useEffect called");
-  });
+  },[]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+  <button>Click Me</button>
+  <DogPics />
+  </div >
+  )
 }
 
 export default App;
